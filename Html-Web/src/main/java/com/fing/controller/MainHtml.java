c6332a62c2253295933c3ee5e3da355e28dc63c1
StefanyaSM <stefanyasalm@gmail.com>
2026-02-12 13:43:40 -0600

    Creacion carpetas y asi

---
+++ Html-Web/src/main/java/com/fing/controller/MainHtml.java
@@ -0,0 +1,5 @@
+package com.fing.controller;
+
+public class MainHtml {
+
+}
---
+++ Html-Web/src/test/java/com/fing/HtmlWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
