25e0fed300d2a8b10f78774e932057aad57276ad
StefanyaSM <stefanyasalm@gmail.com>
2026-02-12 17:59:47 -0600

    Boton dropdown, el menu azul al 100

--- Html-Web/src/main/java/com/fing/controller/MainHtml.java
+++ Html-Web/src/main/java/com/fing/controller/MainHtml.java
@@ -11,12 +11,15 @@ public class MainHtml {
 
     @GetMapping("/perfil")
     public String info(Model model) {
+    	//model.addAttribute("theme", "blue"); //para mostrar solo el azul
+    	model.addAttribute("theme", "red"); //para mostrar solo el rojo
+
 
         User user = new User(
                 "Diana Stefanya Saldaña Mendoza",
                 "a376952@uach.mx",
                 "https://github.com/StefanyaSM",
-                Arrays.asList("Java","Spring","HTML")
+                Arrays.asList("Morado","Rosa","Azul","Rojo", "Naranja", "Verde", "Amarillo", "Negro", "Blanco", "Cafe")
         );
 
         model.addAttribute("user", user);
